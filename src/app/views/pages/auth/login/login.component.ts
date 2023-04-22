5c744167651b64b18c7b38c8679fb02fda942986
gargabhisar <garg.abhisar@gmail.com>
2023-04-22 19:09:37 +0530

    Login Page changes

--- src/app/views/pages/auth/login/login.component.ts
+++ src/app/views/pages/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,21 +9,38 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  returnUrl: any;
+  submitted = false;
+  loginForm: FormGroup;
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
-    // get return url from route parameters or default to '/'
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.loginForm = this.formBuilder.group({
+      useremail: ['', [Validators.required, Validators.email]],
+      userpassword: ['', [Validators.required, Validators.minLength(8)]],
+      publicationid: ['1']
+    });
   }
 
-  onLoggedin(e: Event) {
-    e.preventDefault();
-    localStorage.setItem('isLoggedin', 'true');
-    if (localStorage.getItem('isLoggedin')) {
-      this.router.navigate([this.returnUrl]);
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
     }
+
+    console.log(this.loginForm.value);
+    
+    //let loginresult = this.webapi.login(this.loginForm.value);
+    // localStorage.setItem('isLoggedin', 'true');
+    // if (localStorage.getItem('isLoggedin')) {
+    //   this.router.navigate([this.returnUrl]);
+    // }
   }
 
 }
